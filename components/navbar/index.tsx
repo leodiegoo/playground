6a4d2c073a600c17c22d667e7a4413b3790b2981
Leonardo Barbosa <leonardo.barbosa@sysmap.com.br>
2022-07-05 15:42:19 -0300

    feat(wip): layout

---
+++ components/navbar/index.tsx
@@ -0,0 +1,39 @@
+import { Col, Container, Link, Row, Spacer } from "@nextui-org/react";
+import { StyledNavContainer, StyledNavMainContainer } from "./styles";
+
+import NextLink from "next/link";
+import dynamic from "next/dynamic";
+
+const Navbar = () => {
+  return (
+    <StyledNavMainContainer id="navbar-container">
+      <StyledNavContainer detached={false} showBlur={true}>
+        <Container
+          lg={true}
+          as="nav"
+          display="flex"
+          wrap="nowrap"
+          alignItems="center"
+        >
+          <Col
+            className="navbar__logo-container"
+            css={{
+              "@mdMax": {
+                width: "100%",
+              },
+            }}
+          >
+            <Row justify="flex-start" align="center">
+              <NextLink href="/">
+                <Link href="/">Playground</Link>
+              </NextLink>
+              <Spacer x={0.4} />
+            </Row>
+          </Col>
+        </Container>
+      </StyledNavContainer>
+    </StyledNavMainContainer>
+  );
+};
+
+export { Navbar };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,60 @@
-import { Button, Container, Text } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Grid,
+  Input,
+  Row,
+  Text,
+} from "@nextui-org/react";
 import type { NextPage } from "next";
+import { Navbar } from "../components/navbar";
 
 const Home: NextPage = () => {
   return (
-    <Container>
-      <Text>testando 123</Text>
-      <Button>Click me</Button>
+    <Container sm>
+      <Navbar />
+      <Card>
+        <Card.Header>
+          <Text h3>Todo List App</Text>
+        </Card.Header>
+        <Card.Body>
+          <Row fluid align="center">
+            <Input
+              css={{ marginRight: 5 }}
+              placeholder="Add Todo"
+              bordered
+              fullWidth
+              size="md"
+              rounded
+            />
+            <Button rounded auto size="md" color="gradient">
+              Add
+            </Button>
+          </Row>
+          <Row css={{ marginTop: 10 }}>
+            <Col>
+              <Row>
+                <Col css={{ flexGrow: 1 }}>Add tRPC</Col>
+                <Col css={{ display: "block", width: "auto" }}>
+                  <Button rounded auto ghost color="success">
+                    Done
+                  </Button>
+                </Col>
+                <Col css={{ display: "block", width: "auto", marginLeft: 5 }}>
+                  <Button rounded auto ghost color="warning">
+                    Remove
+                  </Button>
+                </Col>
+              </Row>
+              <Row>
+                <Col>Add NextUI</Col>
+              </Row>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
     </Container>
   );
 };
